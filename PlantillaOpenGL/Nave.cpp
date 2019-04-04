7d6466ba9c817e8e26b5d9ba7d3082173bb83d4b
Javier Eliud Lizarraga Martin Del Campo <jelmdc@hotmail.com>
2019-04-04 08:55:28 -0700

    Agregar archivos de proyecto.

---
+++ PlantillaOpenGL/Nave.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "nave.h"
+
+Nave::Nave() {
+	angulo = 0.0f;
+	coordenadas = vec3(0.0f, 0.0f, 0.0f);
+	//Establecer el valor como una matriz identidad
+	transformaciones = mat4(1.0f);
+}
+
+void Nave::rotar(Direccion direccion) {
+	float rotacion = velocidadAngular;
+	
+	if (direccion == Direccion::Derecha) {
+		rotacion = -rotacion;
+	}
+	angulo += rotacion;
+	actualizarMatrizTransformacion();
+}
+
+void Nave::avanzar() {
+	vec3 traslacion = vec3(
+		cos(angulo * 3.14159 / 180.0f) * velocidadNormal, //X
+		sin(angulo * 3.14159 / 180.0f) * velocidadNormal, //Y
+		0.0f
+	);
+	coordenadas += traslacion;
+	actualizarMatrizTransformacion();
+}
+
+void Nave::actualizarMatrizTransformacion() {
+	//Partir siempre de una matriz identidad
+	transformaciones = mat4(1.0f);
+	
+	//Aplicar transformaciones
+	transformaciones = translate(transformaciones, coordenadas);
+	transformaciones = rotate(transformaciones, angulo * 3.14159f / 180.0f, vec3(0.0f, 0.0f, 1.0f));
+}
